f452ca44da572cf75063872a1863df1d6c7ed676
tagaymuratov <tagaymuratov@gmail.com>
2019-05-03 22:31:08 +0500

    test2

--- kazsmart/js/index.js
+++ kazsmart/js/index.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-    $('.menuIcon').on('click', function(){
+    $(document).on('click','.menuIcon',function(e){
+        e.preventDefault();
+        $('.menuIcon').toggleClass('menuIcon_b');
+        if($('.nav2').is(':visible')) $('nav').slideUp(250);
+        else $('.nav2').slideDown(250);
+    });
+
+    $(document).on('click','.nav2item',function(e){
+        e.preventDefault();
+        $('.menuIcon').toggleClass('menuIcon_b');
+        $('.nav2').slideUp(250);
+    });
+    
+    /*$('.menuIcon').on('click touchend', function(){
         $(this).toggleClass('menuIcon_b');
         if($('.nav2').is(':visible')) $('nav').slideUp(250);
         else $('.nav2').slideDown(250);
@@ -7,5 +20,5 @@ $(document).ready(function(){
 
     $('.nav2item').on('click', function(){
         $('.menuIcon').click()
-    });
+    });*/
 });
