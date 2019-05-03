15734ed0ee5abf13531d7b0d735c07efc45b61b8
tagaymuratov <tagaymuratov@gmail.com>
2019-05-03 22:21:24 +0500

    test1

--- kazsmart/js/index.js
+++ kazsmart/js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $('.menuIcon').on('click tap', function(){
+    $('.menuIcon').on('click', function(){
         $(this).toggleClass('menuIcon_b');
         if($('.nav2').is(':visible')) $('nav').slideUp(250);
         else $('.nav2').slideDown(250);
     });
 
-    $('.nav2item').on('click tap', function(){
+    $('.nav2item').on('click', function(){
         $('.menuIcon').click()
     });
 });
