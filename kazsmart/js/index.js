cb8686dab07337d30f64f990a5318b480ae794ac
tagaymuratov <tagaymuratov@gmail.com>
2019-05-03 23:26:06 +0500

    test3

--- kazsmart/js/index.js
+++ kazsmart/js/index.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    $(document).on('click','.menuIcon',function(e){
+    
+    $(document).on("click", ".menuIcon", function(e){
         e.preventDefault();
         $('.menuIcon').toggleClass('menuIcon_b');
         if($('.nav2').is(':visible')) $('nav').slideUp(250);
         else $('.nav2').slideDown(250);
     });
 
-    $(document).on('click','.nav2item',function(e){
+    $(document).on('click', '.nav2item', function(e){
         e.preventDefault();
         $('.menuIcon').toggleClass('menuIcon_b');
         $('.nav2').slideUp(250);
